3159bbdd2273da354fd44b0761dcdaf0f047dfe0
John Jordan <johnjordan1985@gmail.com>
2021-04-28 08:13:07 -0700

    feat: add connection to MongoDB "tripcost" DB and collections for "trips" and "expenses"

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const mongo = require('mongodb').MongoClient;
+const express = require("express");
+const mongo = require("mongodb").MongoClient;
+
+const mongoURL = "mongodb://localhost:27017";
+let db;
+let trips;
+let expenses;
+mongo.connect(mongoURL, (err, mongoClient) => {
+  if (err) {
+    console.log("An error occurred when connecting to MongoClient");
+  } else {
+    console.log('connected to MongoDB');
+    db = mongoClient.db('tripcost');
+    trips = db.collection('trips');
+    expenses = db.collection('expenses');
+  }
+});
 
 const app = express();
 
-app.post('/trip', (req, res) => { /* */ });
-app.get('/trips', (req, res) => { /* */ });
-app.post('/expense', (req, res) => { /* */ });
-app.get('/expenses', (req, res) => { /* */ });
+app.post("/trip", (req, res) => { /* */ });
+app.get("/trips", (req, res) => { /* */ });
+app.post("/expense", (req, res) => { /* */ });
+app.get("/expenses", (req, res) => { /* */ });
 
-app.listen(3000, () => console.log('Server ready'));
+app.listen(3000, () => console.log("Server ready"));
